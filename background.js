7cac07a37a8bba74d9db21d12b3c9814da12a922
Shreyas Mandlik <shreyas.mandlik.ctr@springhealth.com>
2024-08-18 21:25:40 +0530

    feat: adds api call to it and styling to the popup

--- background.js
+++ background.js
@@ -3,15 +3,34 @@ console.log("Background script loaded");
 
 chrome.contextMenus.create({
   id: "openPopup",
-  title: "Open Popup",
-  contexts: ["selection"],
+  title: "Find Word Definition",
+  contexts: ["selection", "link"],
 });
 
 chrome.contextMenus.onClicked.addListener((info, tab) => {
   console.log("info: ", info.selectionText);
-  console.log("tab: ", tab);
 
-  chrome.storage.local.set({ selectedText: info.selectionText }, () => {
-    chrome.action.openPopup();
-  });
+  const selectedWord = info.selectionText;
+
+  const apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${encodeURIComponent(
+    selectedWord
+  )}`;
+
+  // Fetch data from the API
+  fetch(apiUrl)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Network response was not ok " + response.statusText);
+      }
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+      chrome.storage.local.set({ selectedText: data }, () => {
+        chrome.action.openPopup();
+      });
+    })
+    .catch((error) => {
+      console.error("There was a problem with the fetch operation:", error);
+    });
 });
--- popup.js
+++ popup.js
@@ -1,9 +1,10 @@
 // @ts-nocheck
-// Establish a connection to the background script
-// When the popup loads, retrieve the stored text
 chrome.storage.local.get("selectedText", (result) => {
-  if (result.selectedText) {
-    console.log("Received selected text in popup: ", result.selectedText);
-    document.getElementById("result").innerText = result.selectedText;
+  if (result) {
+    const data = result.selectedText;
+    console.log("result1: ", data[0].word);
+    console.log("result2: ", data[0].meanings[0].definitions[0].definition);
+    document.getElementById("result").innerText = data[0].word;
+    document.getElementById("definition").innerText =data[0].meanings[0].definitions[0].definition;
   }
 });
